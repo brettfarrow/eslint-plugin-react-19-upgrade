d1de1d944791a155e620387ef91b1ba847daddf8
Brett Farrow <brett@acorncity.com>
2024-05-11 12:22:29 -0600

    Added alternate rule names

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
   rules: {
     "no-default-props": require("./rules/no-default-props"),
     "no-prop-types": require("./rules/no-prop-types"),
+    // equivalent but without the extra dash
+    "no-defaultprops": require("./rules/no-default-props"),
+    "no-proptypes": require("./rules/no-prop-types"),
   },
 };
